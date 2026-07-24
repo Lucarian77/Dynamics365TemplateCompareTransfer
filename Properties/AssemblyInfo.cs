af5c1d7414c8b35913b3ba9d5808df27958ecc7c
Adrian Lucaci <adrian.lucaci@ontario.ca>
2026-07-24 11:16:20 -0400

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Dynamics 365 Template Compare & Transfer")]
+[assembly: AssemblyDescription("Compare and safely transfer document templates between Dataverse environments.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Adrian Lucaci")]
+[assembly: AssemblyProduct("Dynamics 365 Template Compare & Transfer")]
+[assembly: AssemblyCopyright("Copyright © Adrian Lucaci 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("e9f65ca2-4dea-4e60-b68e-50cea67ab867")]
+
+[assembly: AssemblyVersion("1.2026.1.2")]
+[assembly: AssemblyFileVersion("1.2026.1.2")]
